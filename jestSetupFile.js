a543173f8172d81f4517e9ad2b2aa03ab3b80b96
johnvees <yohanesvelly23@gmail.com>
2022-04-20 01:12:27 +0700

    add mock for redux persist

--- jestSetupFile.js
+++ jestSetupFile.js
@@ -6,3 +6,13 @@ jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 jest.mock('react-native-share', () => ({default: jest.fn()}));
 jest.mock('react-native-gesture-handler', () => ({default: jest.fn()}));
 jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
+
+jest.mock('redux-persist', () => {
+  const real = jest.requireActual('redux-persist');
+  return {
+    ...real,
+    persistReducer: jest
+      .fn()
+      .mockImplementation((config, reducers) => reducers),
+  };
+});
